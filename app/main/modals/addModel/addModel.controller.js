a2037c380c7223b2b9eb5f14b1d934631c93340c
Mike Lambert <lambert8@illinois.edu>
2018-05-07 13:02:59 -0500

    'Add Model' view is coming together

--- app/main/modals/addModel/addModel.controller.js
+++ app/main/modals/addModel/addModel.controller.js
@@ -9,19 +9,46 @@ angular
  * @author lambert8
  * @see https://opensource.ncsa.illinois.edu/confluence/display/~lambert8/3.%29+Controllers%2C+Scopes%2C+and+Partial+Views
  */
-.controller('ExportJsonCtrl', [ '$scope', '$log', '$uibModalInstance', '_', 'clipboard', 'results', 'title',
-    function($scope, $log, $uibModalInstance, _, clipboard, results, title) {
+.controller('AddModelCtrl', [ '$scope', '$log', '$uibModalInstance', '_',
+    function($scope, $log, $uibModalInstance, _) {
   "use strict";
   
-  $scope.title = title;
+  $scope.newInput = '';
+  $scope.newOutput = '';
+  $scope.modelArgsString = ''; // TODO: Parse this into an array on submit
+  
   $scope.newModel = {
+    // model metadata
+    label: '',
+    name: '',
+    description: '',
+    icon: '',
+    
+    // execution info
+    driver: '',
+    args: [],
+    sourcedir: '',
+    cmakeargs: '',
+    makefile: '',
+    makedir: '',
+    
+    // runtime info
     inputs: [],
     outputs: [],
   };
 
   $scope.submit = function() {
     $log.debug("Closing modal with success!");
-    $uibModalInstance.dismiss('cancel');
+    let model = $scope.newModel;
+    
+    // TODO: Convert args string into array
+    if (!$scope.modelArgsString.args || $scope.modelArgsString.args.replace(/ /g,'') === '') {
+      model.args = null;
+    } else {
+      model.args = _.split($scope.modelArgsString.args, ' ');
+    }
+    
+    $uibModalInstance.close(model);
   };
 
   $scope.close = function() {
