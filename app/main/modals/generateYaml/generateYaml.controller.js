59a96e5cca213ac183267a3962d8fcbab6133f6f
Mike Lambert <lambert8@illinois.edu>
2018-07-24 20:03:44 +0000

    Clean up the modal code, now that the API handles the heavy lifting

--- app/main/modals/generateYaml/generateYaml.controller.js
+++ app/main/modals/generateYaml/generateYaml.controller.js
@@ -16,50 +16,7 @@ angular
   "use strict";
   
   $scope.title = title;
-  $scope.results = angular.copy(results);
-  
-  $scope.legacy = false;
-  
-  $scope.reformat = function(legacy) {
-    let toYaml = { models: angular.copy(results.nodes) };
-    if (!legacy) {
-        toYaml.connections = angular.copy(results.connections);
-    } else {
-      const edges = angular.copy(results.edges);
-      angular.forEach(toYaml.models, function(node) {
-        // Coerce to array if necessary
-        let inputs = angular.isArray(node.inputs) ? node.inputs : [node.inputs]; 
-        let outputs = angular.isArray(node.outputs) ? node.outputs : [node.outputs];
-        
-        let transformedInputs = [];
-        angular.forEach(inputs, function(input) {
-          let edge = _.find(edges, (edge) => edge.destination.label === input);
-          if (edge) {
-            transformedInputs.push({
-              name: edge.destination.label,
-              driver: edge.type || 'RMQInputDriver',
-              args: edge.args || edge.id
-            });
-          }
-        });
-        let transformedOutputs = [];
-        angular.forEach(outputs, function(output) {
-          let edge = _.find(edges, (edge) => edge.source.label === output);
-          if (edge) {
-            transformedOutputs.push({
-              name: edge.source.label,
-              driver: edge.type || 'RMQOutputDriver',
-              args: edge.args || edge.id
-            });
-          }
-        });
-        
-        node.inputs = transformedInputs;
-        node.outputs = transformedOutputs;
-      });
-    }
-    return jsyaml.safeDump(toYaml);
-  };
+  $scope.results = results;
     
   $scope.copy = function() {
     if (!clipboard.supported) {
