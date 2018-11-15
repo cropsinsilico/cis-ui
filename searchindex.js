0b41ff683c6506574bc29bc3e397f04b8dfc0120
Craig Willis <willis8@illinois.edu>
2018-11-15 15:24:10 -0600

    Added docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Crops in Silico\u2019s documentation!"],titleterms:{crop:0,document:0,indic:0,silico:0,tabl:0,welcom:0}})
